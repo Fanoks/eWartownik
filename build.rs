9128fcb770d9d76a641e68b4752355d2839a0e51
Jakub Konieczny <126960714+Fanoks@users.noreply.github.com>
2025-10-06 23:51:54 +0200

    build: adjust build.rs

--- build.rs
+++ build.rs
@@ -1,3 +1,5 @@
-fn main() {
-    slint_build::compile("ui/app-window.slint").expect("Slint build failed");
+fn main() -> () {
+    let config: slint_build::CompilerConfiguration = slint_build::CompilerConfiguration::new().with_bundled_translations("lang").with_style("native".to_string());
+
+    slint_build::compile_with_config("ui/app.slint", config).unwrap();
 }
